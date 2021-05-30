530073387a787ec8302560e389cf77b676f9a20f
young <mould837@naver.com>
2021-05-31 00:05:41 +0900

    0530 제어문 연습

---
+++ src/com/javeex/parctice/Ex01.java
@@ -0,0 +1,30 @@
+package com.javeex.parctice;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		//10에서 20 사이의 정수인 난수 X의 값을 맞추시오
+				
+		int iX = (int)(Math.random() * 10 +10);
+		int iAnswer = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("10에서 20 사이의 정수 x를 맞추세요");
+		
+		while(iAnswer != iX) {
+			System.out.print(">> ");
+			iAnswer = sc.nextInt();
+			if(iAnswer != iX) {
+				System.out.println("정답이 아닙니다.");	
+			}
+		}
+		
+		System.out.println("정답입니다.");
+		
+		sc.close();
+		
+	}
+}
---
+++ src/com/javeex/parctice/Ex10.java
@@ -0,0 +1,20 @@
+package com.javeex.parctice;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		// 1부터 20 미만의 정수 중에서 2 또는 3의 배수인 수의 총합을 구하시오.
+		
+		int iSum = 0;
+		
+		for(int i = 0; i < 20; i++) {
+			if(i % 2 == 0 || i % 3 == 0) {
+				iSum = iSum + i;
+			}
+		}
+		
+		System.out.println(iSum);
+
+	}
+
+}
