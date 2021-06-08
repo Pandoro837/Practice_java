0718a6e89fe712a21761c291ddaf23ea4998863b
young <mould837@naver.com>
2021-06-08 16:47:37 +0900

    프로그래머스 레벨 1

---
+++ src/com/javaex/practice02/Ex01.java
@@ -0,0 +1,32 @@
+package com.javaex.practice02;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자1: ");
+		int x = sc.nextInt();
+		System.out.print("숫자2: ");
+		int n = sc.nextInt();
+		
+		int[] iArray = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			iArray[i] = x + (x * i);
+		}
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println(iArray[i]);
+		}
+		
+		
+		sc.close();
+		
+	}
+	
+	
+}
---
+++ src/com/javaex/practice02/Ex02App.java
@@ -0,0 +1,19 @@
+package com.javaex.practice02;
+
+public class Ex02App {
+
+	public static void main(String[] args) {
+		
+		Ex02 ex = new Ex02();
+		
+		int[] arr = new int[4];
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+		}
+		
+		System.out.println(ex.solution(arr));
+		
+		
+	}
+
+}
