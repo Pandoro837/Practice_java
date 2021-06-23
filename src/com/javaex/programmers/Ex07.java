0992889548dff923af0d0711d04d73421837c851
young <mould837@naver.com>
2021-06-23 17:44:42 +0900

    하샤드 수

--- src/com/javaex/programmers/Ex07.java
+++ src/com/javaex/programmers/Ex07.java
@@ -22,21 +22,30 @@ public class Ex07 {
 			i = i / 10;
 			
 		}
-		
-		if(x % iSum == 0) {
-			System.out.println(true);
-		} else {
-			System.out.println(false);
-		}
+//		
+//		if(x % iSum == 0) {
+//			System.out.println(true);
+//		} else {
+//			System.out.println(false);
+//		}
 		
 //		System.out.println(harshad(x));
 		
+//		int iSum = 0;	//하샤드 수의 각 자릿수의 합
+//		
+//		String str = Integer.toString(x);	//int값을 스트링으로 변환
+//		char[] cArray = str.toCharArray();	//해당 값을 자릿수별로 쪼개어 문자형 배열에 저장
+//		
+//		for(int i = 0; i < cArray.length; i ++) {
+//			//자릿수의 합을 배열 내부 문자형의 int 형 변환으로 구함
+//			iSum+= Character.getNumericValue(cArray[i]);	
+//			}
+//		
+//		//해당 수를 자릿수의 합으로 나눈 나머지의 값을 확인
+//		if(x % iSum == 0) {	
+//			System.out.println(true);
+//		} else {
+//			System.out.println(false);
+//		}
 	}
-//	
-//	public static boolean harshad(int x) {
-//		
-//		
-//		
-//	}
-
 }
