3d1a953565b3f7c496cb5f64e87865c8f95fadd3
young <mould837@naver.com>
2021-06-26 18:36:52 +0900

    0626

--- src/com/javaex/programmers/Ex11.java
+++ src/com/javaex/programmers/Ex11.java
@@ -8,17 +8,17 @@ public class Ex11 {
 
 	public static int solution (int n) {
 		int answer = 0;
-		String str = Integer.toString(n);
-		String[] sArr = str.split("");
-		int[] iArr = new int[sArr.length];
-		for(int i = 0; i < sArr.length; i++) {
-			iArr[i] = Integer.parseInt(sArr[i]);
-		}
-		
-		for(int i = 0; i < iArr.length; i++) {
-			answer+=iArr[i];
-		}
-		
+//		String str = Integer.toString(n);
+//		String[] sArr = str.split("");
+//		int[] iArr = new int[sArr.length];
+//		for(int i = 0; i < sArr.length; i++) {
+//			iArr[i] = Integer.parseInt(sArr[i]);
+//		}
+//		
+//		for(int i = 0; i < iArr.length; i++) {
+//			answer+=iArr[i];
+//		}
+//		
 		return answer;
 	}
 	
